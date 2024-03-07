4325c3b195c63a23873b76e0efdfd504c1c225db
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2024-03-07 17:17:13 +0100

    Create test to check the functionality of the hit method

--- src/test/java/e1/LogicTest.java
+++ src/test/java/e1/LogicTest.java
@@ -1,15 +1,21 @@
 package e1;
+
 import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 public class LogicTest {
 
-  @Test
-  public void test() {
-    assert(true);
-    // TODO: Add your test logic here
-    // You can generate random inputs and assert the expected output
-    // For example:
-    // int result = Logic.someMethod(5, 10);
-    // assertEquals(expectedResult, result);
-  }
+    @Test
+    public void testKnightHit() {
+        Pair<Integer, Integer> knightPos = new Pair<>(2, 2);
+        Pair<Integer, Integer> pawnPos = new Pair<>(0, 3);
+        Logics logics = new LogicsImpl(5, pawnPos, knightPos);
+
+        assertTrue(logics.hit(pawnPos.getX(), pawnPos.getY()));
+        assertFalse(logics.hit(knightPos.getX(), knightPos.getY()));
+        assertFalse(logics.hit(0, 0));
+        assertFalse(logics.hit(1, 1));
+    }
+
 }
